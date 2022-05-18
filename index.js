22d418cb0fa4cf0901e126cf1d702bac13d095af
Win Feigle <win.feigle@gmail.com>
2022-05-17 19:23:50 -0600

    commit tree 54b05fe8ae206c8f2d0352693c70c16b83b997a5

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
+const num1 = 31;
+const num2 = 2;
+const num3 = 16;
+const num4 = 4;
 
-// Write your code here
+
+
+let multiply = num1 * num2;
+
+const random = Math.floor(Math.random() * 10);
+
+const mod = num3/num4;
+
+const max = Math.max(3, 7, 14, 18, 19, 20);
